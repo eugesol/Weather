1b2d5a1baf8f096b796bf8070fbc8dba06e98d4e
Eugenia <eugeniasol13@gmail.com>
2020-07-26 18:42:41 -0400

    5day cards

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var lat = "";
 var lng = "";
 var citiesArray = [];
 
-$("#search").on("click",function(){
+$("#search").on("click", function () {
     event.preventDefault();
     city = $("#inputCity").val();
     currentWeather();
@@ -12,26 +12,26 @@ $("#search").on("click",function(){
 
 
 
-function currentWeather(){
-    
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log("response:", response);
-            $("#cityName").text(response.name);
-            saveCities(response);
-            console.log(citiesArray);
-            lat = response.coord.lat;
-            lng = response.coord.lon;
-            oneCall();
+function currentWeather() {
 
-        });
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log("response:", response);
+        $("#cityName").text(response.name);
+        saveCities(response);
+        console.log(citiesArray);
+        lat = response.coord.lat;
+        lng = response.coord.lon;
+        oneCall();
+
+    });
 
 }
 
-function oneCall(){
+function oneCall() {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&appid=" + APIKey;
     $.ajax({
@@ -41,29 +41,41 @@ function oneCall(){
         console.log("oneCallResponse:", response);
         var temp = Math.floor((response.current.temp - 273.15) * 1.80 + 32);
         $("#temp").text("Temperature: " + temp);
-        $("#humid").text("Humidity: "+ response.current.humidity);
-        $("#wind").text("Wind Speed: "+ response.current.wind_speed);
-        $("#uvi").text("UV Index: "+response.current.uvi);
+        $("#humid").text("Humidity: " + response.current.humidity);
+        $("#wind").text("Wind Speed: " + response.current.wind_speed);
+        $("#uvi").text("UV Index: " + response.current.uvi);
         displayBtns();
-            $(".cityBtn").on("click",function(){
-                event.preventDefault();
-                city = $(this).text();
-                currentWeather();
-                console.log($(this).text())
-                console.log("click")
-            })
+        $(".cityBtn").on("click", function () {
+            event.preventDefault();
+            city = $(this).text();
+            currentWeather();
+            console.log($(this).text())
+            console.log("click")
+        });
+        for (i = 1; i < 6; i++) {
+            var dayCard = $("#forecast")
+            dayCard.append(`
+            <div class="card text-white bg-primary mb-3" id="fiveDay"  style="max-width: 13rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                <p class="card-text" id="temp">Temperature: ${response.daily[i].temp.day}</p>
+                <p class="card-text" id="humid">Humidity: ${response.daily[i].humidity}</p>
+                </div>
+            </div>
+            `)
+        }
     });
 }
 
-function saveCities(object){
-    if (citiesArray.includes(object.name)){
+function saveCities(object) {
+    if (citiesArray.includes(object.name)) {
         return;
-    }else{
+    } else {
         citiesArray.push(object.name)
     }
-}   
+}
 
-function displayBtns(){
+function displayBtns() {
     var btnArea = $(".btnGroup");
     btnArea.empty();
     citiesArray.forEach(element => {
@@ -71,7 +83,7 @@ function displayBtns(){
         cityBtn.text(element);
         btnArea.append(cityBtn);
         console.log(element);
-        
+
     })
 
 }
